6921ef4dc6d525e872121962e7f4cfac07f883de
erebrus <erebrus@gmail.com>
2023-05-02 14:06:33 +0200

    fixes to sound on game over/win layers

--- src/Map.gd
+++ src/Map.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
-	
+func _ready() -> void:
+	Globals.connect("survived", self, "on_game_over")
+	Globals.connect("game_over", self, "on_game_over")
+		
 func _on_Timer_timeout() -> void:
 	Globals.time.tick()
 	
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Globals.emit_signal("game_over")
+
+
+func on_game_over():
+	var tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property($Music, "volume_db", -80, 5)
+
--- src/ui/HUD.gd
+++ src/ui/HUD.gd
@@ -1,20 +1,25 @@
 extends CanvasLayer
 
+
 func _ready() -> void:
 	Globals.connect("survived", self, "show_win")
 	Globals.connect("game_over", self, "show_lose")
 
+
 func show_win():
 	$Win.show()
+	$sfx_win.play()
 	fade()
 	
 func show_lose():
 	$Lose.show()
+	$sfx_lose.play()
 	fade()
-	
+
+		
 func fade():
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	var panel = $Panel
+	var panel = $Panel	
 	tween.tween_property(panel, "color", Color.black, 5)
 	yield(tween,"finished")
 	get_tree().change_scene("res://src/ui/MainMenu.tscn")
